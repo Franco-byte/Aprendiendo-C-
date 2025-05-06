295bcf790fb0b90214c64ba90b49671719b580dd
Franco-byte <fd.reyes3110@gmail.com>
2025-05-06 03:34:47 -0300

    Calculator

---
+++ MyApp/calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Calculator
+{
+    static int Main() {
+
+        string operation;
+
+        while (true) {
+            Console.WriteLine("Select an operation: Add/Subtract/Multiply/Split");
+            operation = Console.ReadLine() ?? "";
+            if (operation == "Add" || operation == "Subtract" || operation == "Multiply" || operation == "Split") {
+                break;
+            }
+            Console.WriteLine("No descanses gil, hasta te los escribi en la seleccion");
+        }
+
+        int numA;
+
+        while (true) {    
+            Console.WriteLine("First number: int only");
+            string input = Console.ReadLine() ?? "";
+            if (int.TryParse(input, out numA)) {
+                break;
+            }
+            Console.WriteLine("Salame manda bien las cosas");
+        }
+
+        int numB;
+
+        while (true) {
+            Console.WriteLine("Second number: int only");
+            string input = Console.ReadLine() ?? "";
+            if (int.TryParse(input, out numB)) {
+                break;
+            }
+            Console.WriteLine("Salame manda bien las cosas");
+        }
+
+        int result;
+
+        if (operation == "Add") {
+            result = Operations.Add(numA, numB);
+        }
+        else if (operation == "Subtract") {
+            result = Operations.Subtract(numA, numB);
+        }
+        else if (operation == "Multiply") {
+            result = Operations.Multiply(numA, numB);
+        }
+        else if (operation == "Split") {
+            result = Operations.Split(numA, numB);
+        }
+        else {
+            Console.WriteLine("Invalid operation");
+            return 1;
+        }
+        Console.WriteLine($"Result:\n{result}");
+        return 0;
+    }
+}
---
+++ MyApp/operators.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Operations
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+    public static int Split(int a, int b)
+    {
+        return a / b;
+    }
+}
